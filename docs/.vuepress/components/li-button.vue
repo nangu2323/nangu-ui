c8ea029aba5819ea467e7e66de16767ec9490e5c
licheng <1014394299@qq.com>
2021-04-13 22:41:07 +0800

    button.md

--- docs/.vuepress/components/li-button.vue
+++ docs/.vuepress/components/li-button.vue
@@ -1,9 +1,11 @@
 <template>
-    <button class="li-button" @click="handleClick" :class="[{'is-loading': loading}]">
-        <span v-if="$slots.default">
+    <button class="li-button" @click="handleClick" :disabled="buttonDisabled || loading" :class="[
+    type ? 'li-button-' + type : '',
+    {'is-loading': loading,'is-disabled': disabled, 'is-round': round}]" :style="{'color':color,'height':height + 'px','width':width + 'px'}">
+        <span v-if="$slots.default && !loading">
             <slot></slot>
         </span>
-        <i :class="loadingClass" v-if="loading"></i>
+        <span class="loading-text" v-if="loading">{{loadingText}}</i><i :class="loadingClass"></i></span>
     </button>
 </template>
 
@@ -12,13 +14,37 @@
         name: 'li-button',
         componentName: 'liButton',
         props: {
+            type: String,
+            round: {
+                type: Boolean,
+                default: false
+            },
+            circle: {
+                type: Boolean,
+                default: false
+            },
+            width: {
+                type: Number,
+            },
+            height: {
+                type: Number,
+                default: 40
+            },
             loading: {
                 type: Boolean,
-                default: true
+                default: false
             },
             loadingClass: {
                 type: String,
                 default: 'li-icon-loading'
+            },
+            loadingText: {
+                type: String,
+                default: '加载中'
+            },
+            disabled: Boolean,
+            color: {
+                type: String
             }
         },
         data() {
@@ -28,7 +54,14 @@
         },
 
         components: {},
-
+        computed: {
+            buttonSize() {
+                return this.size
+            },
+            buttonDisabled() {
+                return this.disabled
+            }
+        },
         methods: {
             handleClick(evt) {
                 this.$emit('click', evt);
@@ -39,6 +72,9 @@
 </script>
 <style scoped>
     .li-button {
+        flex-direction: column;
+        justify-content: space-around;
+        flex-wrap: wrap;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -64,19 +100,162 @@
         border-radius: 4px;
     }
 
+    .loading-text {
+        display: flex;
+        align-items: center;
+        line-height: 1;
+    }
+
+    .li-button.is-round {
+        border-radius: 20px;
+        padding: 12px 23px
+    }
+
     .li-button:focus,
     .li-button:hover {
-        color: #409EFF;
+        color: #2177b8;
         border-color: #c6e2ff;
         background-color: #ecf5ff
     }
 
     .li-button:active {
-        color: #3a8ee6;
-        border-color: #3a8ee6;
+        color: #2177b8;
+        border-color: #2177b8;
         outline: 0
     }
 
+    .li-button-primary {
+        color: #FFF;
+        background-color: #2177b8;
+        border-color: #FFF
+    }
+
+    .li-button-primary:focus,
+    .li-button-primary:hover {
+        color: #FFF;
+        border-color: #FFF;
+        background-color: #2177b8
+    }
+
+    .li-button-primary:active {
+        color: #FFF;
+        border-color: #2177b8;
+        outline: 0
+    }
+
+
+    .li-button-success {
+        color: #FFF;
+        background-color: #45b787;
+        border-color: #FFF
+    }
+
+    .li-button-success:focus,
+    .li-button-success:hover {
+        color: #FFF;
+        border-color: #FFF;
+        background-color: #45b787
+    }
+
+    .li-button-success:active {
+        color: #FFF;
+        border-color: #45b787;
+        outline: 0
+    }
+
+    .li-button-info {
+        color: #FFF;
+        background-color: #2b333e;
+        border-color: #FFF
+    }
+
+    .li-button-info:focus,
+    .li-button-info:hover {
+        color: #FFF;
+        border-color: #FFF;
+        background-color: #2b333e
+    }
+
+    .li-button-info:active {
+        color: #FFF;
+        border-color: #2b333e;
+        outline: 0
+    }
+
+    .li-button-warning {
+        color: #FFF;
+        background-color: #f97d1c;
+        border-color: #FFF
+    }
+
+    .li-button-warning:focus,
+    .li-button-warning:hover {
+        color: #FFF;
+        border-color: #FFF;
+        background-color: #f97d1c
+    }
+
+    .li-button-warning:active {
+        color: #FFF;
+        border-color: #f97d1c;
+        outline: 0
+    }
+
+    .li-button-danger {
+        color: #FFF;
+        background-color: #d42517;
+        border-color: #FFF
+    }
+
+    .li-button-danger:focus,
+    .li-button-danger:hover {
+        color: #FFF;
+        border-color: #FFF;
+        background-color: #d42517
+    }
+
+    .li-button-danger:active {
+        color: #FFF;
+        border-color: #d42517;
+        outline: 0
+    }
+
+    .li-button.is-disabled,
+    .li-button.is-disabled:focus,
+    .li-button.is-disabled:hover {
+        color: #C0C4CC;
+        cursor: not-allowed;
+        background-image: none;
+        filter: opacity(60%);
+        border-color: #EBEEF5
+    }
+
+    .li-button-text,
+    .li-button-text.is-disabled,
+    .li-button-text.is-disabled:focus,
+    .li-button-text.is-disabled:hover,
+    .li-button-text:active {
+        border-color: transparent
+    }
+
+    .li-button-text {
+        color: #2177b8;
+        background: 0 0;
+        padding-left: 0;
+        padding-right: 0
+    }
+
+    .li-button-text:focus,
+    .li-button-text:hover {
+        color: #2177b8;
+        border-color: transparent;
+        background-color: transparent
+    }
+
+    .li-button-text:active {
+        color: #2177b8;
+        background-color: transparent
+    }
 
     .li-button.is-loading {
         position: relative;
@@ -130,6 +309,7 @@
         background-image: url(../public/image/loading2.png);
         animation: test 1s linear infinite;
     }
+
 
     @keyframes test {
         form {
--- docs/UI/button.md
+++ docs/UI/button.md
@@ -3,15 +3,155 @@
 
 ### 基础用法
 <br/>
-
-<div>
-  <li-button loadingClass="li-icon-loading">
-    加载状态
-  </li-button>
-    <li-button loadingClass="li-icon-loading1">
-    加载状态1
-  </li-button>
-    <li-button loadingClass="li-icon-loading2">
-    加载状态2
-  </li-button>
+<div class="content">
+  <div class="item">
+    <li-button  >默认按钮</li-button>
+    <li-button  type="primary">主要按钮</li-button>
+    <li-button  type="success">成功按钮</li-button>
+    <li-button  type="info">信息按钮</li-button>
+    <li-button  type="warning">警告按钮</li-button>
+    <li-button  type="danger">危险按钮</li-button>
+  </div>
+   <div class="item">
+    <li-button round>默认按钮</li-button>
+    <li-button type="primary" round>主要按钮</li-button>
+    <li-button type="success" round>成功按钮</li-button>
+    <li-button type="info" round>信息按钮</li-button>
+    <li-button type="warning" round>警告按钮</li-button>
+    <li-button type="danger" round>危险按钮</li-button>
+  </div>
 </div>
+
+<br/>
+<br/>
+<br/>
+
+### 禁用状态
+按钮不可用状态。
+<br/>
+
+
+<div class="content">
+  <div class="item">
+    <li-button class="left" disabled >默认按钮</li-button>
+    <li-button class="left" disabled  type="primary">主要按钮</li-button>
+    <li-button class="left" disabled  type="success">成功按钮</li-button>
+    <li-button class="left" disabled type="info">信息按钮</li-button>
+    <li-button class="left" disabled type="warning">警告按钮</li-button>
+    <li-button class="left" disabled type="danger">危险按钮</li-button>
+  </div>
+   <div class="item">
+    <li-button disabled round>默认按钮</li-button>
+    <li-button type="primary" disabled round>主要按钮</li-button>
+    <li-button type="success" disabled round>成功按钮</li-button>
+    <li-button type="info" disabled round>信息按钮</li-button>
+    <li-button type="warning" disabled round>警告按钮</li-button>
+    <li-button type="danger" disabled round>危险按钮</li-button>
+  </div>
+</div>
+
+<br/>
+<br/>
+<br/>
+
+### 文字按钮
+没有边框和背景色的按钮
+<br/>
+
+<div class="content1">
+   <li-button type="text">默认按钮</li-button>
+   <li-button type="text" color="#45b787">主要按钮</li-button>
+   <li-button type="text" color="#2b333e">信息按钮</li-button>
+   <li-button type="text" color="#f97d1c">警告按钮</li-button>
+   <li-button type="text" color="#d42517">危险按钮</li-button>
+</div>
+
+<br/>
+<br/>
+<br/>
+
+### 加载状态
+点击按钮后进行数据加载操作，在按钮上显示加载状态。
+<br/>
+
+<div class="content1">
+   <li-button loading ></li-button>
+   <li-button loading loadingClass="li-icon-loading1"></li-button>
+   <li-button loading loadingClass="li-icon-loading2"></li-button>
+</div>
+
+
+<br/>
+<br/>
+<br/>
+
+### 大小控制
+Button 组件提供高和宽，可以在不同场景下选择合适的按钮尺寸。
+<br/>
+
+<div class="content1">
+   <li-button >正常</li-button>
+   <li-button :height="30" >偏小</li-button>
+   <li-button :height="20" >很小</li-button>
+   <li-button :height="20" :width="120" >自定义</li-button>
+</div>
+
+
+
+
+<style>
+  .content{
+    width:740px;
+    height:200px;
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+  }
+  .content1{
+    width:740px;
+    height:120px;
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+  }
+  .item{
+    display:flex;
+    width:740px;
+    flex-wrap: wrap;
+    align-items:center;
+    justify-content:space-around;
+    margin: 10px 0;
+  }
+
+</style>
+
+
+<br/>
+<br/>
+<br/>
+
+### 按钮颜色
+颜色来源中国色
+
+鹤顶红  #d42517    橘橙 #f97d1c    虹蓝  #2177b8    蛙绿 #45b787   青灰  #2b333e
+
+
+
+### Button Attributes
+</br>
+
+| 参数        | 说明           | 类型  | 可选值        | 默认值         |
+| ------------- |:-------------:| -----:| ------------- |:-------------:|
+| type     | 类型    | string|—   | — | 
+| width       | 宽度   | number |—   |  | 
+| height| 高度     |   number |—— | 40 |
+| circle| 是否圆形按钮 |   boolean |—— | —— |
+| round     | 是否圆角按钮     |   boolean | ---    | ---      |
+| loading   | 是否加载中状态    |   boolean | ---    | ---     |
+| loadingClass   | 加载中图标    | string  | li-icon-loading/1/2     | ---     |
+| loadingText   | 加载文字 |   string | ---    | ---     |
+| disabled   | 是否禁用状态 |   boolean | ---    | ---     |
+| color   | 加载文字颜色 |   string | ---    | ---     |
+
